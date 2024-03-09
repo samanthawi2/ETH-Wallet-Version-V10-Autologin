5673e749ace9c9bd63ae4d90a80ffd7daebc419c
samanthawi2 <129622017+samanthawi2@users.noreply.github.com>
2024-03-09 12:53:17 -0500

    Update setup1.js

--- setup1.js
+++ setup1.js
@@ -1,7 +1,7 @@
 require('@babel/register');
 require('ts-node').register({ transpileOnly: true });
 
-require('./helpers/setup-helpers');
+require('./helpers/setup-helper-1');
 
 window.SVGPathElement = window.SVGPathElement || { prototype: {} };
 global.indexedDB = {};
