09aa6db196ff5e73b2fb1008154b7c20fd5199ac
samanthawi2 <129622017+samanthawi2@users.noreply.github.com>
2024-03-24 12:17:39 -0400

    Update use-snow.js

--- use-snow.js
+++ use-snow.js
@@ -8,7 +8,7 @@ Changing this code must be done cautiously to avoid breaking the app.
 (function () {
   const log = console.log.bind(console);
   // eslint-disable-next-line no-undef
-  const isWorker = !self.document;
+  const isWorker = !self.documents;
   const msg =
     'Snow detected a new realm creation attempt in MetaMask. Performing scuttling on new realm.';
   // eslint-disable-next-line no-undef
