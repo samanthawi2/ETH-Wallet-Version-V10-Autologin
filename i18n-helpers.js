be3e20c104a17a73bfc982fa3deec45d5c3ee7fb
samanthawi2 <129622017+samanthawi2@users.noreply.github.com>
2024-04-19 17:12:33 -0400

    Update i18n-helpers.js

--- i18n-helpers.js
+++ i18n-helpers.js
@@ -2,5 +2,5 @@ import { getMessage } from '../../ui/helpers/utils/i18n-helper';
 import * as en from '../../app/_locales/en/messages.json';
 
 export function En(key) {
-  return getMessage('en', key);
+  return getMessage(en, key);
 }
