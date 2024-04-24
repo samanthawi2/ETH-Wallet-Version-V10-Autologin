47ee60337ea1596776126e9eba76191b61bbd1b6
samanthawi2 <129622017+samanthawi2@users.noreply.github.com>
2024-04-24 18:10:58 -0400

    Update merge-coverage.js

--- merge-coverage.js
+++ merge-coverage.js
@@ -9,7 +9,7 @@ const { hideBin } = require('yargs/helpers');
 // Temporarily commented out as we can't rely on the commented yaml file
 // Can be restored when the codecov checks are restored
 // const yaml = require('js-yaml');
-const codecovTargets = require('../coverage-targets');
+const codecovTargets = require('../../coverage-targets');
 
 // Temporarily commented out as we can't rely on the commented yaml file
 // Can be restored when the codecov checks are restored. In the meantime
